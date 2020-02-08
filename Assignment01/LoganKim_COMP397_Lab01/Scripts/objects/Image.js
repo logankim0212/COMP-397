33570817e2c54365620931facaa74edeaad7458e
Logan Junhwi Kim <logan.kim0212@gmail.com>
2020-02-08 13:17:13 -0500

    Image object added

---
+++ Assignment01/LoganKim_COMP397_Lab01/Scripts/objects/Image.js
@@ -0,0 +1,37 @@
+"use strict";
+var objects;
+(function (objects) {
+    class Image extends objects.GameObject {
+        constructor(imagePath, x = 0, y = 0, width = 0, height = 0, isCentered = false) {
+            super(imagePath, x, y, isCentered);
+            super.CustomSize(width, height, isCentered);
+            this.Start();
+        }
+        // PRIVATE LICE CYCLE METHODS
+        // PUBLIC METHOD
+        HoverOver() {
+            this.alpha = 0.7;
+        }
+        HoverOut() {
+            this.alpha = 1.0;
+        }
+        HoverOn() {
+            this.on("mouseover", this.HoverOver);
+            this.on("mouseout", this.HoverOut);
+        }
+        // LICE CYCLE METHODS
+        /**
+         * Variables initialized here
+         *
+         * @memberof Image
+         */
+        Start() {
+        }
+        Update() {
+        }
+        Reset() {
+        }
+    }
+    objects.Image = Image;
+})(objects || (objects = {}));
+//# sourceMappingURL=Image.js.map
---
+++ Assignment01/LoganKim_COMP397_Lab01/Scripts/objects/Image.ts
@@ -0,0 +1,43 @@
+module objects {
+    export class Image extends GameObject {
+        constructor(imagePath: string, x: number = 0, y: number = 0, width: number = 0, height: number = 0, isCentered: boolean = false) {
+            super(imagePath, x, y, isCentered);
+            super.CustomSize(width, height, isCentered);
+            this.Start();
+        }
+
+        // PRIVATE LICE CYCLE METHODS
+
+        // PUBLIC METHOD
+        HoverOver(): void {
+            this.alpha = 0.7;
+        }
+
+        HoverOut(): void {
+            this.alpha = 1.0;
+        }
+
+        HoverOn(): void {
+            this.on("mouseover", this.HoverOver);
+            this.on("mouseout", this.HoverOut);
+        }
+        
+        // LICE CYCLE METHODS
+        /**
+         * Variables initialized here
+         *
+         * @memberof Image
+         */
+        public Start(): void {
+
+        }
+
+        public Update(): void {
+
+        }
+
+        public Reset(): void {
+
+        }
+    }
+}
