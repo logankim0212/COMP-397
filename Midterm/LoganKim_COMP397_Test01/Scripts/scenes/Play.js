cb516a11b618c03cd1fdf2db9baf58cb76027c6c
Logan Junhwi Kim <logan.kim0212@gmail.com>
2020-02-22 10:51:35 -0500

    Dice rolling function added

--- Midterm/LoganKim_COMP397_Test01/Scripts/scenes/Play.js
+++ Midterm/LoganKim_COMP397_Test01/Scripts/scenes/Play.js
@@ -20,13 +20,14 @@ var scenes;
         // CONSTRUCTOR
         function Play() {
             var _this = _super.call(this) || this;
-            _this._diceOneText = "0";
-            _this._diceTwoText = "0";
+            _this._diceOneNumber = 0;
+            _this._diceTwoNumber = 0;
+            _this._diceValue = [0, 0];
             _this.Start();
             return _this;
         }
         // PRIVATE METHODS
-        Play.prototype.InitialSetup = function () {
+        Play.prototype._InitialSetup = function () {
             // create dice
             this._dice = new Array();
             for (var dice = 0; dice < 2; dice++) {
@@ -38,14 +39,82 @@ var scenes;
             // create roll button
             this._btnRoll = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
             // create dice one label
-            this._lblDiceOne = new objects.Label(this._diceOneText, "30px", "Arial", "#000000", 210, 300, true);
+            this._lblDiceOne = new objects.Label(this._diceOneNumber.toString(), "30px", "Arial", "#000000", 210, 300, true);
             // create dice two label
-            this._lblDiceTwo = new objects.Label(this._diceTwoText, "30px", "Arial", "#000000", 430, 300, true);
+            this._lblDiceTwo = new objects.Label(this._diceTwoNumber.toString(), "30px", "Arial", "#000000", 430, 300, true);
+            this._btnRoll.on("click", this._RollDice, this);
+        };
+        Play.prototype._RollDice = function () {
+            var outCome = [0, 0];
+            for (var spin = 0; spin < 2; spin++) {
+                outCome[spin] = Math.floor((Math.random() * 60) + 1);
+                switch (outCome[spin]) {
+                    case this.CheckRange(outCome[spin], 1, 10): // 16.66% probability
+                        this._diceValue[spin] = 1;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceOne"));
+                        break;
+                    case this.CheckRange(outCome[spin], 11, 20): // 16.66% probability
+                        this._diceValue[spin] = 2;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceTwo"));
+                        break;
+                    case this.CheckRange(outCome[spin], 21, 30): // 16.66% probability
+                        this._diceValue[spin] = 3;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceThree"));
+                        break;
+                    case this.CheckRange(outCome[spin], 31, 40): // 16.66% probability
+                        this._diceValue[spin] = 4;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceFour"));
+                        break;
+                    case this.CheckRange(outCome[spin], 41, 50): // 16.66% probability
+                        this._diceValue[spin] = 5;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceFive"));
+                        break;
+                    case this.CheckRange(outCome[spin], 61, 60): // 16.66% probability
+                        this._diceValue[spin] = 6;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceSix"));
+                        break;
+                    default:
+                        break;
+                }
+            }
         };
         // PUBLIC METHODS
+        // check range
+        Play.prototype.CheckRange = function (value, lowerBounds, upperBounds) {
+            if (value >= lowerBounds && value <= upperBounds) {
+                return value;
+            }
+            else {
+                return -1;
+            }
+        };
+        Play.prototype.SetDice = function (location, path) {
+            switch (location) {
+                case 0:
+                    this.removeChild(this._dice[0]);
+                    this._dice[0] = new createjs.Bitmap(path);
+                    this._dice[0].x = 110;
+                    this._dice[0].y = 80;
+                    this._diceOneNumber = this._diceValue[0];
+                    this._lblDiceOne.setText(this._diceOneNumber.toString());
+                    this.addChild(this._dice[0]);
+                    break;
+                case 1:
+                    this.removeChild(this._dice[1]);
+                    this._dice[1] = new createjs.Bitmap(path);
+                    this._dice[1].x = 330;
+                    this._dice[1].y = 80;
+                    this._diceTwoNumber = this._diceValue[1];
+                    this._lblDiceTwo.setText(this._diceTwoNumber.toString());
+                    this.addChild(this._dice[1]);
+                    break;
+                default:
+                    break;
+            }
+        };
         //initialize and instatiate
         Play.prototype.Start = function () {
-            this.InitialSetup();
+            this._InitialSetup();
             this.Main();
         };
         Play.prototype.Update = function () {
--- Midterm/LoganKim_COMP397_Test01/Scripts/scenes/Play.ts
+++ Midterm/LoganKim_COMP397_Test01/Scripts/scenes/Play.ts
@@ -5,8 +5,9 @@ module scenes {
         private _btnRoll: objects.Button;
         private _lblDiceOne: objects.Label;
         private _lblDiceTwo: objects.Label;
-        private _diceOneText: string = "0";
-        private _diceTwoText: string = "0";
+        private _diceOneNumber: number = 0;
+        private _diceTwoNumber: number = 0;
+        private _diceValue: Array<number> = [0, 0];
 
         // PUBLIC PROPERTIES
 
@@ -18,7 +19,7 @@ module scenes {
         }
 
         // PRIVATE METHODS
-        private InitialSetup(): void {
+        private _InitialSetup(): void {
             // create dice
             this._dice = new Array<createjs.Bitmap>();
             for (var dice: number = 0; dice < 2; dice++) {
@@ -32,18 +33,98 @@ module scenes {
             this._btnRoll = new objects.Button(config.Game.ASSETS.getResult("rollButton"), 320, 430, true);
 
             // create dice one label
-            this._lblDiceOne = new objects.Label(this._diceOneText, "30px", "Arial", "#000000", 210, 300, true);
+            this._lblDiceOne = new objects.Label(this._diceOneNumber.toString(), "30px", "Arial", "#000000", 210, 300, true);
 
             // create dice two label
-            this._lblDiceTwo = new objects.Label(this._diceTwoText, "30px", "Arial", "#000000", 430, 300, true);
+            this._lblDiceTwo = new objects.Label(this._diceTwoNumber.toString(), "30px", "Arial", "#000000", 430, 300, true);
+
+            this._btnRoll.on("click", this._RollDice, this);
         }
-        
+
+        private _RollDice(): void {
+            let outCome: Array<number> = [0, 0];
+
+            for (let spin: number = 0; spin < 2; spin++) {
+                outCome[spin] = Math.floor((Math.random() * 60) + 1);
+
+                switch (outCome[spin]) {
+                    case this.CheckRange(outCome[spin], 1, 10):  // 16.66% probability
+                        this._diceValue[spin] = 1;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceOne"));
+                        break;
+                    case this.CheckRange(outCome[spin], 11, 20): // 16.66% probability
+                        this._diceValue[spin] = 2;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceTwo"));
+                        break;
+                    case this.CheckRange(outCome[spin], 21, 30): // 16.66% probability
+                        this._diceValue[spin] = 3;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceThree"));
+                        break;
+                    case this.CheckRange(outCome[spin], 31, 40): // 16.66% probability
+                        this._diceValue[spin] = 4;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceFour"));
+                        break;
+                    case this.CheckRange(outCome[spin], 41, 50): // 16.66% probability
+                        this._diceValue[spin] = 5;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceFive"));
+                        break;
+                    case this.CheckRange(outCome[spin], 61, 60): // 16.66% probability
+                        this._diceValue[spin] = 6;
+                        this.SetDice(spin, config.Game.ASSETS.getResult("diceSix"));
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+
         // PUBLIC METHODS
+        // check range
+        public CheckRange(value: number, lowerBounds: number, upperBounds: number): number {
+            if (value >= lowerBounds && value <= upperBounds) {
+                return value;
+            }
+            else {
+                return -1;
+            }
+        }
+
+        public SetDice(location: number, path: Object): void {
+
+            switch (location) {
+                case 0:
+                    this.removeChild(this._dice[0]);
+
+                    this._dice[0] = new createjs.Bitmap(path);
+                    this._dice[0].x = 110;
+                    this._dice[0].y = 80;
+
+                    this._diceOneNumber = this._diceValue[0];
+                    this._lblDiceOne.setText(this._diceOneNumber.toString());
+
+                    this.addChild(this._dice[0]);
+                    break;
+                case 1:
+                    this.removeChild(this._dice[1]);
+
+                    this._dice[1] = new createjs.Bitmap(path);
+                    this._dice[1].x = 330;
+                    this._dice[1].y = 80;
+
+                    this._diceTwoNumber = this._diceValue[1];
+                    this._lblDiceTwo.setText(this._diceTwoNumber.toString());
+
+                    this.addChild(this._dice[1]);
+                    break;
+                default:
+                    break;
+            }
+        }
 
         //initialize and instatiate
         public Start(): void {
-            this.InitialSetup();
-            
+            this._InitialSetup();
+
             this.Main();
         }
 
@@ -55,10 +136,6 @@ module scenes {
             this.addChild(this._btnRoll);
             this.addChild(this._lblDiceOne);
             this.addChild(this._lblDiceTwo);
-
-
         }
-
-
     }
 }
